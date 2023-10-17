d35453fb2ee5426e3faccd0956e0bf37b65d6d58
Joydip <joydip.mondal@gmail.com>
2023-10-17 14:44:09 +0530

    angular introduction to components and binding upload

--- codes/day-2/first-ng-app/src/app/app.module.ts
+++ codes/day-2/first-ng-app/src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  //here you register components, directives and pipes
+  declarations: [AppComponent, HomeComponent],
+  //you register ONLY services here
   providers: [],
-  bootstrap: [AppComponent]
+  //you register ONLY modules (built-in angular modules and application feature modules) here
+  imports: [BrowserModule],
+  //mention which of the registered component(s) should be by default displayed in index.html file body element, when the application runs in the browser
+  bootstrap: [
+    AppComponent,
+    //HomeComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+
+}
